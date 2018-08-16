4e988b1c90c7e4db82b335e6cb9b25f28382fcfb
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-08-16 16:18:26 +0530

    fetch Keyword from Search box and display places starting with that keyword.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 var ViewModel = function() {
     var self = this;
+    this.filter = ko.observable('');
 
+    this.filteredList = ko.computed(function () {
+        var result = [];
+        locations.forEach(function (location) {
+            if(self.filter() === null){
+                result.push(location)
+            }
+            else if(location.title.toLowerCase().startsWith(self.filter().toLowerCase())){
+                result.push(location)
+            }
+        });
+        return result;
+    })
 };
 
 ko.applyBindings(new ViewModel());
