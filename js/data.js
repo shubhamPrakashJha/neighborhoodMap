2c3ab5988768dd6ebb44b28c4957260e5471060c
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-08-16 11:48:18 +0530

    Add Location Data in the App

---
+++ js/data.js
@@ -0,0 +1,76 @@
+/**
+ * Created by Shubham Prakash Jha on 15-08-2018.
+ */
+var locations = [
+    {
+        title: 'Buddha Smriti Park',
+        location: {
+            lat: 25.606396,
+            lng: 85.137062
+        }
+    },
+    {
+        title: 'Golghar',
+        location: {
+            lat: 25.620346,
+            lng: 85.139429
+        }
+    },
+    {
+        title: 'Patna Zoo',
+        location: {
+            lat: 25.597593,
+            lng: 85.100845
+        }
+    },
+    {
+        title: 'Shrikrishna Science Centre',
+        location: {
+            lat: 25.616419,
+            lng: 85.141108
+        }
+    },
+    {
+        title: 'ISKON Temple',
+        location: {
+            lat: 25.606811,
+            lng: 85.133422
+        }
+    },
+    {
+        title: 'Planetarium',
+        location: {
+            lat: 25.610284,
+            lng: 85.131796
+        }
+    },
+    {
+        title: 'Patna Museum',
+        location: {
+            lat: 25.612653,
+            lng: 85.133232
+        }
+    },
+    {
+        title: 'Patna Sahib Gurdwara',
+        location: {
+            lat: 25.595947,
+            lng: 85.230065
+        }
+    },
+    {
+        title: 'Floating Restaurant',
+        location: {
+            lat: 25.622221,
+            lng: 85.172609
+        }
+    },
+    {
+        title: 'Revolving Restaurant',
+        location: {
+            lat: 25.617460,
+            lng: 85.140709
+        }
+    }
+
+];
