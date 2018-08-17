9432cbdbc2ca643bc73dd2d0bd134bcaaf77bbff
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-08-18 01:16:19 +0530

    Organised Code According to MVVM framework.

---
+++ js/mapStyle.js
@@ -0,0 +1,87 @@
+/**
+ * Created by Shubham Prakash Jha on 18-08-2018.
+ */
+var styles = [
+    {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+    {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+    {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+    {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+    },
+    {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+    },
+    {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{color: '#3aa724'}]
+    },
+    {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#6b9a76'}]
+    },
+    {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{color: '#617287'}]
+    },
+    {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#212a37'}]
+    },
+    {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9ca5b3'}]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{color: '#fae3b6'}]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#1f2835'}]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#f3d19c'}]
+    },
+    {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{color: '#5d708d'}]
+    },
+    {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+    },
+    {
+        featureType: 'transit.station',
+        stylers: [{color: '#d59563'}, {weight: 9}]
+    },
+    {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{color: '#6395ff'}]
+    },
+    {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#515c6d'}]
+    },
+    {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{color: '#17263c'}]
+    }
+];
