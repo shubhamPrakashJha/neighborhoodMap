e2f92722372bbfc91e33ef852e1e9bccbc549fd1
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-08-19 03:41:51 +0530

    Add comment in the application

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
+// variable storing google Maps Mao object
 var map;
+// List of markers stored during map initialization
 var markers = [];
 
+//handle error if map does not load
 function mapInitError() {
     window.alert("Error occurred while loading google maps... ");
 }
 
+// Initializes google Maps API
 function initMap() {
 
+    // create Map having center as 'Patna' and Having custom style
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: 25.594095,
@@ -17,13 +22,18 @@ function initMap() {
         // mapTypeControl: false
     });
 
+    // create boundaries to store all markers inside the display area
     var bounds = new google.maps.LatLngBounds();
+    //create info window having width 300
     var largeInfoWindow = new google.maps.InfoWindow({
         maxWidth: 300
     });
+
+    //image of marker icons used to highliht markers when hovering
     var defaultIcon = makeMarkerIcon('red-dot');
     var highlightedIcon = makeMarkerIcon('blue-dot');
 
+    // Create Markers foe Each Location
     locations.forEach(function (place) {
         place.marker = new google.maps.Marker({
             title: place.title,
@@ -32,11 +42,13 @@ function initMap() {
             position: place.location
         });
 
+        // push each marker to markers array
         markers.push({
             title: place.title,
             marker: place.marker
         });
 
+        // when markers is clicked call funtion to display info window while animating
         place.marker.addListener('click',function () {
             place.marker.setAnimation(google.maps.Animation.BOUNCE);
             setTimeout(function () {
@@ -44,8 +56,9 @@ function initMap() {
             }, 1400);
             populateInfoWindow(this, largeInfoWindow)
         });
-        place.marker.setIcon(defaultIcon);
 
+        //  call function to change default icon and highlight marker while hovering
+        place.marker.setIcon(defaultIcon);
         place.marker.addListener('mouseover', function () {
             this.setIcon(highlightedIcon);
         });
@@ -53,27 +66,39 @@ function initMap() {
             this.setIcon(defaultIcon);
         });
 
+        // Extend bound so that all markers fit inside the boundaries
         bounds.extend(place.marker.position);
         map.fitBounds(bounds);
     });
 
+    // lastly apply Knockout View Model Bindings
     ko.applyBindings(new ViewModel());
 }
 
+
+// function to set up info window when marker is clicked
 function populateInfoWindow(marker, infowindow) {
+    // map infowindow to clicked marker if not already connected
     if (infowindow.marker !== marker) {
         infowindow.marker = marker;
+        // delete previous markers content
         infowindow.setContent('');
+        // disconnect info window from marker if user clicks X
         infowindow.addListener('click', function () {
             infowindow.setMarker(null);
         });
+        // create StreetViewService object to get use street view image for places
         var streetViewService = new google.maps.StreetViewService();
+        //set radius of location of street image from the clicked places
         var radius = 50;
 
+        // if wikipedia content failed to load within 8 sec, display error
         var wikiRequestTimeout = setTimeout(function () {
             infowindow.setContent('failed to load wikipedia resource');
         }, 8000);
 
+        // ajax call to mediawiki API to fetch info from wikipedia and
+        // attach it to infowindow of the clicked marker.
         var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="+marker.title;
         $.ajax({
             url: wikiUrl,
@@ -91,12 +116,15 @@ function populateInfoWindow(marker, infowindow) {
             }
         });
 
+        // function to fetch street view image from google maps
         function getStreetView(data, status) {
             if (status === google.maps.StreetViewStatus.OK) {
+                //nearest location of street view image from the site
                 var nearStreetViewLocation = data.location.latLng;
+                // compute heading to point the image in the direction of monuments
                 var heading = google.maps.geometry.spherical.computeHeading(
                     nearStreetViewLocation, marker.position);
-                // infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+                // settings for  panorama options
                 var panoramaOptions = {
                     position: nearStreetViewLocation,
                     pov: {
@@ -104,16 +132,23 @@ function populateInfoWindow(marker, infowindow) {
                         pitch: 30
                     }
                 };
+
+                //obtain panorama image and appending it to '#pano' element
                 var panorama = new google.maps.StreetViewPanorama(
                     document.getElementById('pano'), panoramaOptions);
             } else {
+                // catch error if street view image is not displayed
                 $('#pano').text('Street View Image Not Available')
             }
         }
+
+        // now after attaching wiki info and panorama image to infowindow
+        // open infowindow in the map near the clicked marker
         infowindow.open(map, marker);
     }
 }
 
+// function to change marker icon
 function makeMarkerIcon(markerColor) {
     var markerImage = {
         url: "http://maps.google.com/mapfiles/ms/icons/"+markerColor+".png",
@@ -125,22 +160,36 @@ function makeMarkerIcon(markerColor) {
     return markerImage;
 }
 
+// knockout View Model
 var ViewModel = function() {
+    // store this in a var to refer to ViewModel Observables
+    // when context changed while using with or foreach in the view
     var self = this;
+    // stores keyword entered by the user in the input box
     this.filter = ko.observable('');
 
+    // when a place in list is clicked
+    //trigger a click event as if its corrosponding marker is clicked
     this.animateMarker = function (location) {
         google.maps.event.trigger(location.marker, 'click');
     };
 
+    // compute the list of markers matching user input in the filter
+    // and make them visible
     this.filteredList = ko.computed(function () {
+        // convert keyword to lowercase
         var filter = self.filter().toLowerCase();
+        //if filter is empty make all the markers visible
+        //and return the entire lost of location to html view
         if (filter === null) {
             locations.forEach(function (place) {
                 place.marker.setVisible(true);
             });
             return locations;
         }
+        // if filter is present filterout location which includes filter keyword
+        //    store it in an array and return this new array
+        //    containing list of keyword matched location to the html view
         else {
             var result = [];
             locations.forEach(function (place) {
